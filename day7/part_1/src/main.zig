f224655bbfd784a712a4f6edb29ec3e0e2729f4d
luuk <lvan-gef@student.codam.nl>
2024-12-08 01:20:04 +0100

    Less allocs

--- day7/part_1/src/main.zig
+++ day7/part_1/src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const data = @embedFile("input.txt");
 const stdout = std.io.getStdOut().writer();
 
+const MAX_NUMBERS = 12;
+const MAX_OPERATIONS = MAX_NUMBERS - 1;
+
 const Operator = enum {
     add,
     multiply,
@@ -14,24 +17,19 @@ const Operator = enum {
     }
 };
 
-fn findSolution(allocator: std.mem.Allocator, target: usize, numbers: []const usize) !bool {
+fn findSolution(operators: []Operator, target: usize, numbers: []const usize) bool {
     const num_operators = numbers.len - 1;
-
-    var operators = try allocator.alloc(Operator, num_operators);
-    defer allocator.free(operators);
-
     const max_combinations = std.math.pow(usize, 2, num_operators);
     var combination: usize = 0;
 
     while (combination < max_combinations) : (combination += 1) {
-        var temp = combination;
-        for (operators, 0..) |_, i| {
-            operators[i] = if (temp & 1 == 0) .add else .multiply;
-            temp >>= 1;
+        const temp = combination;
+        for (operators[0..num_operators]) |*op| {
+            op.* = @enumFromInt(temp % 2);
         }
 
         var result = numbers[0];
-        for (operators, 0..) |op, i| {
+        for (operators[0..num_operators], 0..) |op, i| {
             result = op.apply(result, numbers[i + 1]);
         }
 
@@ -48,21 +46,26 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    var inputs = try std.ArrayList(usize).initCapacity(allocator, MAX_NUMBERS);
+    defer inputs.deinit();
+
+    var operations: [MAX_OPERATIONS]Operator = undefined;
+
     var result: usize = 0;
     var it = std.mem.tokenizeScalar(u8, data, '\n');
     while (it.next()) |line| {
+        inputs.clearRetainingCapacity();
+
         var tokens = std.mem.tokenizeScalar(u8, line, ' ');
         const nbr = tokens.next().?;
         const target = try std.fmt.parseInt(usize, nbr[0 .. nbr.len - 1], 10);
-
-        var inputs = std.ArrayList(usize).init(allocator);
-        defer inputs.deinit();
 
         while (tokens.next()) |token| {
             try inputs.append(try std.fmt.parseInt(usize, token, 10));
         }
 
-        if (try findSolution(allocator, target, inputs.items)) {
+        std.debug.assert(inputs.items.len <= MAX_NUMBERS);
+        if (findSolution(&operations, target, inputs.items)) {
             result += target;
         }
     }
