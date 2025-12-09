f340588407e0caab340bd0efee6b9a1e4de5db8e
Luuk van Geffen <lvan-gef@student.codam.nl>
2025-12-09 03:02:50 +0100

    No idea how todo part 2

--- 2025/day7/src/main.c
+++ 2025/day7/src/main.c
@@ -34,11 +34,9 @@ typedef struct queue_s {
 } queue_t;
 
 static size_t part1(const map_t *map, queue_t *queue);
-static size_t part2(const map_t *map, queue_t *queue);
 static void get_size(const filemap_t *fm, map_t *map);
 static bool init(const filemap_t *fm, map_t *map, queue_t *queue);
 static void get_start_pos(const map_t *map, node_t *node);
-static void reset_queue(queue_t *queue, const map_t *map);
 static void free_prog(filemap_t *fm, map_t *map, queue_t *queue);
 static void free_grid(char **grid);
 static void free_queue(queue_t *queue);
@@ -68,62 +66,13 @@ int main(int argc, char **argv) {
 
     size_t result1 = part1(&map, &queue);
 
-    reset_queue(&queue, &map);
-    size_t result2 = part2(&map, &queue);
-
     printf("Part: 1: Beam splitting: %zu\n", result1);
-    printf("Part: 2: Beam timelines: %zu\n", result2);
     free_prog(&fm, &map, &queue);
 
     return 0;
 }
 
 static size_t part1(const map_t *map, queue_t *queue) {
-    node_t start = {0};
-
-    get_start_pos(map, &start);
-    queue->visited[start.row][start.col] = true;
-    queue->nodes[queue->tail] = start;
-    ++queue->tail;
-
-    size_t result = 0;
-    while (queue->head != queue->tail) {
-        node_t cur = queue->nodes[queue->head];
-        ++queue->head;
-
-        ssize_t r = cur.row;
-        ssize_t c = cur.col;
-
-        while ((size_t)r < map->row) {
-            if (map->grid[r][c] == '^') {
-                if (!queue->visited[r][c]) {
-                    queue->visited[r][c] = true;
-                    ++result;
-                }
-
-                if (c - 1 >= 0 && !queue->visited[r][c - 1]) {
-                    node_t left = {.row = r, .col = c - 1};
-                    queue->nodes[queue->tail] = left;
-                    ++queue->tail;
-                    queue->visited[r][c - 1] = true;
-                }
-
-                if ((size_t)(c + 1) < map->col && !queue->visited[r][c + 1]) {
-                    node_t right = {.row = r, .col = c + 1};
-                    queue->nodes[queue->tail] = right;
-                    ++queue->tail;
-                    queue->visited[r][c + 1] = true;
-                }
-                break;
-            }
-            ++r;
-        }
-    }
-
-    return result;
-}
-
-static size_t part2(const map_t *map, queue_t *queue) {
     node_t start = {0};
 
     get_start_pos(map, &start);
@@ -250,28 +199,13 @@ static void get_start_pos(const map_t *map, node_t *node) {
         size_t col = 0;
         while (col < map->col) {
             if (map->grid[row][col] == 'S') {
-                node->row = (ssize_t)row;
+                node->row = (ssize_t)row + 1;
                 node->col = (ssize_t)col;
                 return;
             }
             ++col;
         }
 
-        ++row;
-    }
-}
-
-static void reset_queue(queue_t *queue, const map_t *map) {
-    queue->head = 0;
-    queue->tail = 0;
-
-    size_t row = 0;
-    while (row < map->row) {
-        size_t col = 0;
-        while (col < map->col) {
-            queue->visited[row][col] = false;
-            ++col;
-        }
         ++row;
     }
 }
